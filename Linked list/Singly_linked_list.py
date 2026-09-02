4ff40eafdc4b350bd4d1e2896827ee7fda368685
eshanpadhiar <eshanpadhiar3@gmail.com>
2026-09-02 13:06:16 +0530

    Singly linked list till insertion

---
+++ Linked list/Singly_linked_list.py
@@ -0,0 +1,51 @@
+class node:
+               def __init__(self, data):
+                       self.data = data
+                       self.next = None
+
+class singlylinkedlist:
+               def __init__(self):
+                       self.head = None
+
+
+               def prepend(self, data):
+                       new = node(data)
+
+                       if self.head == None:
+                              self.head = new
+                              return
+                       
+                       new.next = self.head
+                       self.head = new
+
+               def append(self, data):
+                       new = node(data)
+
+                       if self.head == None:
+                               self.head = new
+                               return
+                       
+                       current = self.head
+                       while current.next is not None:
+                               current = current.next
+                       current.next = new
+
+               def insert_after_pos(self, data, target):
+                       if self.head == None:
+                               print('List is Empty, insertion can\' be performed')
+                               return
+
+                       current = self.head
+                       while current is not None and current.data != target:
+                               current = current.next
+
+                       if current is None:
+                               return 'Target element not found in the list'
+
+                       new = current.next
+                       current.next = new
+
+                       
+
+                       
+                               
