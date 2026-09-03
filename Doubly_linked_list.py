167f41eabf5a6caf11aee05999dac562a1e6c8a6
eshanpadhiar <eshanpadhiar3@gmail.com>
2026-09-03 20:57:36 +0530

    basic structure of linked list

---
+++ Doubly_linked_list.py
@@ -0,0 +1,10 @@
+class node:
+               def __init__(self, data):
+                              self.data = data
+                              self.next = None
+                              self.prev = None
+
+class doubly_linked_list:
+               def __init__(self):
+                       self.head = None
+                       self.tail = None
